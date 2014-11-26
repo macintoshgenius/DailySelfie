d08afe928f32176feb55efbfc8698222eab2d4d2
Willow Cheng <e.wingus@Gmail.com>
2014-11-25 22:14:08 -0500

    Add action bar icon

--- src/willowcheng/postach/io/dailyselfie/ItemListActivity.java
+++ src/willowcheng/postach/io/dailyselfie/ItemListActivity.java
@@ -2,6 +2,8 @@ package willowcheng.postach.io.dailyselfie;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.app.Activity;
 
 /**
@@ -73,5 +75,24 @@ public class ItemListActivity extends Activity implements
 			detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
 			startActivity(detailIntent);
 		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_camera) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
